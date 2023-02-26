c60cab594d5e9ef5a567a33d1219172e78783c43
Jordan Leslie <jordan02leslie@gmail.com>
2023-02-26 00:26:43 -0500

    updated datetime component

--- components/CustomDateTimePicker.tsx
+++ components/CustomDateTimePicker.tsx
@@ -1,109 +1,50 @@
-import DateTimePicker from "@react-native-community/datetimepicker";
 import React, { useState } from "react";
-import { StyleSheet, Platform, TouchableOpacity } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+import { View, Button } from "./Themed";
 import { convertDate, convertTime } from "../firebase/ConvertPostTypes";
 
-import { Text, View } from "./Themed";
+const CustomDateTimePicker = ({ date, setDate, mode }: { date: Date, setDate: any, mode: "date" | "time" }) => {
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-export default function CustomDateTimePicker({
-    onChangeStart,
-    start,
-    onChangeEnd,
-    end,
-}: {
-    onChangeStart: any;
-    start: Date;
-    onChangeEnd: any;
-    end: Date;
-}) {
-    const [mode, setMode] = useState("date");
-    const [show, setShow] = useState(false);
-    const [selected, setSelected] = useState("start");
-    const onChange = (event: any, selectedValue?: Date) => {
-        setShow(false);
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
     };
 
-    const showMode = (mode: string, select: string) => {
-        setSelected(select);
-        setShow(true);
-        setMode(mode);
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
     };
-    return (
-        <View>
-            {Platform.OS === "ios" && (
-                <View style={styles.container}>
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={start}
-                        mode="date"
-                        is24Hour={false}
-                        display="default"
-                        onChange={onChangeStart}
-                        minimumDate={new Date()}
-                    />
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={start}
-                        mode="time"
-                        is24Hour={false}
-                        display="default"
-                        minuteInterval={5}
-                        onChange={onChangeStart}
-                    />
-                    <Text> to </Text>
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={end}
-                        mode="time"
-                        is24Hour={false}
-                        display="default"
-                        minuteInterval={5}
-                        onChange={onChangeEnd}
-                    />
-                </View>
-            )}
-            {Platform.OS !== "ios" && (
-                <View>
-                    <TouchableOpacity onPress={() => showMode("date", "start")}>
-                        <Text>{convertDate(start)}</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => showMode("time", "start")}>
-                        <Text>{convertTime(start)}</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => showMode("time", "end")}>
-                        <Text>{convertTime(end)}</Text>
-                    </TouchableOpacity>
-                </View>
-            )}
-            {Platform.OS !== "ios" && show && (
-                <View>
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={start}
-                        mode={mode === "date" ? "date" : "time"}
-                        is24Hour
-                        display="default"
-                        onChange={(event) => {
-                            if (selected === "start") {
-                                onChangeStart();
-                                onChange(event, start);
-                            } else {
-                                onChangeEnd();
-                                onChange(event, end);
-                            }
-                        }}
-                    />
-                </View>
-            )}
-        </View>
-    );
-}
 
-const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-    },
-});
+    if (mode === "date") {
+        return (
+            <View>
+                <Button color="green" title={convertDate(date)} onPress={showDatePicker} />
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible}
+                    mode={mode}
+                    date={date}
+                    onConfirm={hideDatePicker}
+                    onChange={setDate}
+                    onCancel={hideDatePicker}
+                    minimumDate={new Date()}
+                />
+            </View>
+        );
+    }
+    else return (
+        <>
+            <Button color="green" title={convertTime(date)} onPress={showDatePicker} />
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible}
+                    date={date}
+                    mode={mode}
+                    onConfirm={hideDatePicker}
+                    onChange={setDate}
+                    onCancel={hideDatePicker}
+                    minuteInterval={5}
+                />
+        </>
+    )
+};
+
+export default CustomDateTimePicker;
