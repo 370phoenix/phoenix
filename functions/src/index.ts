2e9dcf67750062e5255153efa763480c0985eea1
Jordan Leslie <jordan02leslie@gmail.com>
2023-04-12 18:10:09 -0400

    fixed outdated removePosts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,7 +9,7 @@ export const ridersChangedNotification = functions.database
     .ref("/posts/{postID}/riders")
     .onUpdate((change, context) => onRidersChanged(change, context));
 
-export const clearCompleted = functions.pubsub
+    export const clearCompleted = functions.pubsub
     .schedule("0 0 * * *")
     .timeZone("America/New_York")
     .onRun(async () => {
@@ -28,82 +28,3 @@ export const clearOnDemand = functions.https.onRequest(async (_req, res) => {
         res.status(500).send("Error removing posts");
     }
 });
-
-async function deletePost(postID: string, userID: string, userInfo: UserInfo) {
-    try {
-        console.log(userInfo);
-        const db = admin.database();
-
-        const postRef = db.ref("posts/" + postID);
-        await postRef.remove();
-
-        let newPosts = userInfo.posts ? userInfo.posts : [];
-        if (newPosts.length == 1) newPosts = [];
-        else {
-            const i = newPosts.indexOf(postID);
-            if (i !== -1) newPosts.splice(i, 1);
-        }
-        userInfo.posts = newPosts;
-
-        const res = await writeUser(userID, userInfo);
-
-        if (typeof res === "string") throw Error(res);
-
-        return true;
-    } catch (e: any) {
-        return e.message;
-    }
-}
-
-async function writeUser(userID: string, userInfo: UserInfo) {
-    try {
-        if (!userID || !userInfo) throw new Error("No User ID or Info.");
-
-        const db = admin.database();
-        const userRef = db.ref("users/" + userID);
-        await userRef.set(cleanUndefined(userInfo));
-        return true;
-    } catch (e: any) {
-        return e.message;
-    }
-}
-
-export async function getUserOnce(userID: string | null): Promise<UserInfo> {
-    try {
-        const db = admin.database();
-        if (!userID) throw Error("No user ID.");
-        const userRef = db.ref("users/" + userID);
-        const snapshot = await userRef.once("value");
-        console.log(snapshot.exists());
-        if (snapshot.exists()) return snapshot.val();
-        throw Error("No info stored");
-    } catch (e: any) {
-        return e.message;
-    }
-}
-
-type UserInfo = {
-    username: string;
-    phone: string;
-    major: string;
-    gradYear: number;
-    pronouns: string;
-    chillIndex: number | undefined;
-    ridesCompleted: number;
-    posts: string[] | undefined;
-    pending: string[] | undefined;
-    matches: string[] | undefined;
-    requests: [string, string][];
-};
-
-type Clean<T> = {
-    [K in keyof T]?: any;
-};
-
-function cleanUndefined<T extends object>(obj: T): T {
-    let clean: Clean<T> = {};
-    for (const k in obj) {
-        if (obj[k]) clean[k] = obj[k];
-    }
-    return clean as T;
-}
