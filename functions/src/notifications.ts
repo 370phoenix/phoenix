d7e20faf28eb59880c87e80c0df7bce1fe2b0954
Jordan Leslie <jordan02leslie@gmail.com>
2023-04-13 19:54:15 -0400

    refactor

--- functions/src/notifications.ts
+++ functions/src/notifications.ts
@@ -8,10 +8,38 @@ import * as admin from "firebase-admin";
 
 const db = admin.database();
 
+export async function onRidersChanged(change: any, context: any) {
+    try {
+        const { postID } = context.params;
+
+        // before = rider array before update, after = rider array after update
+        const before = change.before.val();
+        const after = change.after.val();
+
+        // more riders than before, someone has matched
+        if (after.length > before.length) {
+            // notify the person who has matched (userID which is in after but not before)
+            const newRiders = after.filter((x: string) => !before.includes(x));
+            for (const newRider of newRiders) {
+                sendAcceptNotification(postID, newRider);
+            }
+        }
+
+        // fewer riders than before, someone has cancelled
+        else if (after.length < before.length) {
+            sendCancelNotifications(postID);
+        }
+        return true;
+    } catch (e: any) {
+        console.error(`Error in DELETE POSTS: ${e.message}`);
+        return false;
+    }
+}
+
 /**
  * Send push notifications for accepted requests when listener detects update
  */
-export async function sendCancelNotifications(postID: string) {
+async function sendCancelNotifications(postID: string) {
     try {
         const snapshot = await db.ref("posts/" + postID).get();
         if (!snapshot) throw Error("Error fetching post\n");
@@ -76,49 +104,20 @@ async function sendOneNotification(expoPushToken: string, title: string, body: s
     });
 }
 
-export async function onRidersChanged(change: any, context: any) {
-    try {
-        const { postID } = context.params;
-
-        // before = rider array before update, after = rider array after update
-        const before = change.before.val();
-        const after = change.after.val();
-
-        // more riders than before, someone has matched
-        if (after.length > before.length) {
-            // notify the person who has matched (userID which is in after but not before)
-            const newRiders = after.filter((x: string) => !before.includes(x));
-            for (const newRider of newRiders) {
-                sendAcceptNotification(postID, newRider);
-            }
-        }
-
-        // fewer riders than before, someone has cancelled
-        else if (after.length < before.length) {
-            sendCancelNotifications(postID);
-        }
-        return true;
-    } catch (e: any) {
-        console.error(`Error in DELETE POSTS: ${e.message}`);
-        return false;
-    }
-}
-
-
 /**
  * Get push token for given user
  *
  * @param userID
  * @returns ExpoPushToken for given user
  */
-export async function getPushToken(userID: string): Promise<any> {
+async function getPushToken(userID: string): Promise<any> {
     try {
         const snapshot = await db.ref("pushTokens/" + userID).once("value");
         const val = snapshot.val();
-        if (snapshot.exists()) return {type: "Success", data: val};
+        if (snapshot.exists()) return { type: "Success", data: val };
         else throw Error("Error fetching push token from DB");
     } catch (e: any) {
-        return {type: "Error", message: `Error: ${e.message}`};
+        return { type: "Error", message: `Error: ${e.message}` };
     }
 }
 
@@ -128,7 +127,7 @@ export async function getPushToken(userID: string): Promise<any> {
  * @param users An array of userIDs
  * @returns array of ExpoPushTokens for given users
  */
-export async function getMultiplePushTokens(users: string[]): Promise<string[]> {
+async function getMultiplePushTokens(users: string[]): Promise<string[]> {
     const tokens: string[] = [];
     for (const user of users) {
         const res = await getPushToken(user);
