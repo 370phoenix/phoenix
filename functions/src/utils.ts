9a88c5357384559c797d0dc2ea57001788dbe033
Jordan Leslie <jordan02leslie@gmail.com>
2023-04-14 14:47:21 -0400

    remove functions from repo

--- functions/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Clean } from "./types";
-
-export function cleanUndefined<T extends object>(obj: T): T {
-    let clean: Clean<T> = {};
-    for (const k in obj) {
-        if (obj[k]) clean[k] = obj[k];
-    }
-    return clean as T;
-}
